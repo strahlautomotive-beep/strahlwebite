bcfb6e20872641914aad408cdbb27912a495af22
Adnan Ali <sayedadnanali905@gmail.com>
2026-08-08 15:31:29 +0530

    Fix black patch issue on footer logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ export default function Footer() {
                 alt="STRAHL Logo"
                 width={100}
                 height={100}
-                style={{ height: "auto", width: "85px", objectFit: "contain" }}
+                style={{ height: "auto", width: "85px", objectFit: "contain", mixBlendMode: "lighten" }}
               />
               <span style={{ fontSize: "14px", fontWeight: "700", marginLeft: "4px", marginTop: "4px", color: "rgba(255, 255, 255, 0.7)" }}>™</span>
             </div>
